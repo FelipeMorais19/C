392e368db8398dd20f7b9cf81022bf54e9476569
FelipeMorais19 <felipemorais192014@gmail.com>
2023-11-02 14:24:10 -0300

    aula 10

--- Aula10/aula10.cs
+++ Aula10/aula10.cs
@@ -1,8 +1,14 @@
 using System;
 
+
 class Aula10{
+
+enum DiasSemana{Domingo, Segunda, Terça, Quarta, Quinta, Sexta, Sábado};
 
     static void Main() {
         
+        int ds = (int)DiasSemana.Sexta;
+
+        Console.WriteLine(ds);
     }
 }
