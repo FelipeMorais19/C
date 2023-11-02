e9e22a5c05ed78372e87c0da7a58f504ea4238d2
Felipe Elias de Morais <120696999+FelipeMorais19@users.noreply.github.com>
2023-11-02 14:03:57 -0300

    Até a aula 10

---
+++ Aula07/aula07.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Aula07 {
+    static void Main() {
+        const string canal = "CFB Cursos";
+        const double pi = 3.1315;
+
+        Console.WriteLine("Canal {0}\nPi: {1}", canal, pi);
+    }
+}
