0d502e673e28eb1158752a027b0777402539bda8
Felipe Elias de Morais <120696999+FelipeMorais19@users.noreply.github.com>
2023-11-07 19:21:22 -0300

    Até a aula 20

--- Aula19/aula19.cs
+++ Aula19/aula19.cs
@@ -2,6 +2,14 @@ using System;
 
 class Aula19 {
     static void Main() {
-        
+        int[] num = new int[50];
+
+        for(int i = 0; i < num.Length; i++) {
+            num[i] = i;
+        }
+
+        for(int i = 0; i < num.Length; i++) {
+            Console.WriteLine("Valor de num na posição {0}: {1}", i, num[i]);
+        }
     }
 }
